f61443ea999ab0a59d5f6393b4901ba449001603
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-10 05:12:23 -0800

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main -print a text
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-  puts("\"Ptogramming is like building a multilingual puzzle");
-  return (0)
-    }
+   puts("\"Ptogramming is like building a multilingual puzzle");
+   return (0)
+ }
+
