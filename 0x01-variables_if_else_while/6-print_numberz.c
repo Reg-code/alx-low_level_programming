a7afdc80aba9909ee93560bc0244b54116cd63fc
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-13 05:45:53 -0700

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <='9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return(0);
+}
+
