f1b3ec2f95975421c6582ba20bbb7c5040a00e96
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-16 21:37:23 +0100

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#imclude "main.h"
+
+/**
+ * print_alphabet - write a function that print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
