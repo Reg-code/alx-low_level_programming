7a0f0feb166da547a6acb5a1a91ddd09111376d0
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-16 13:47:30 +0100

    my commit

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+#!/bin/bash
+low level programming 
+
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,4 +5,10 @@
  * @c: the character to print
  *
  * Return: Always on success 1.
- * On 
+ * On error -1 is returned, and error is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
