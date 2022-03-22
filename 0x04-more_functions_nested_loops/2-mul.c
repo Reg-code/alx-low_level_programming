94199bcc7a76a6e7a6384a6eb142aca325e5374c
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-22 00:08:47 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - fuction that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: mul.
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	return (x);
+}
