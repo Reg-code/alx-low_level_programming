7f07f496167085fa26e66bc859dc838f30ab8579
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-29 08:32:57 -0700

    file5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,21 +11,22 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *result = haystack, *fneedle = needle;
 
-	while(*haystack)
+	while (*haystack)
 	{
-		while(*needle)
+		while (*needle)
 		{
 			if (*haystack++ != *needle++)
 			{
 				break;
 			}
 		}
-		if (! * needle)
+		if (!*needle)
 		{
 			return (result);
 		}
 		needle = fneedle;
 		result++;
 		haystack = result;
-	}return (0);
+	}
+	return (0);
 }
