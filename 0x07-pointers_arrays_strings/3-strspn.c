734a80bfba8e45be0c9d43a78dcc7bc15a2c23d5
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-29 03:08:20 -0700

    My first commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#inclide "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string for checking substring
+ * @accept: Accepted chars bof substring
+ * Return: length of occurrence
+ */
+unsigned int_strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while(*s++)
+	{
+		while (*accept++)
+			if(*(s -1.) = (*accept - 1))
+		{
+			c++;
+			break;
+		}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
+
+					
