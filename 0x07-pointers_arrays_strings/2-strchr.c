7e361f93b346d36f8ec92ab6187bd866d3381c9a
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-29 07:18:02 -0700

    file2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if(*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	if(*s == c)
-	{
-		return (s);
-	}
-	return (0);
-}
+	int i;
 
+	for(i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0')
+}
