97dbc5d0b041865eca0acc01b8da77fdc11bac22
Reg-code <rebeccadurojaiye1@gmail.com>
2022-03-31 02:27:16 -0700

    file5

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * divisors - number is prime?
+ * @n: integer params
+ * @m: integer params
+ * Return: boolean
+ */
+int divisors( int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)	
+	{
+		return (divisors(n + 2, m);
+				}
+				else
+				{
+				return (1);
+				}
+				}
